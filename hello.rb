f103f972a18952b56f36982a029875e321fc348c
Daniel Faddoul <daniel.faddoul@gmail.com>
2017-12-02 19:38:19 +0000

    added my first ruby array to the file

--- hello.rb
+++ hello.rb
@@ -1,3 +1,13 @@
-puts "Hello World!"
+gift = [
+          "half of a leftover burrito",
+          "a day-old cup of coffee",
+          "a tesla",
+          "a life-size quadrocopter",
+          "a slice of stale wedding cake",
+          "a medium-sized island off the coast of Sumatra",
+          "a visual prosthesis",
+         ]
+         
+mySimpleGift = "a single string"
 
-puts "ths is my first feature branch"
+puts "All I got for valentine's day was #{gift.sample}."
