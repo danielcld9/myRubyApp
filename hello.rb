65ae7dd44ac493828fe76d6647e146e51620bc04
Daniel Faddoul <daniel.faddoul@gmail.com>
2017-12-02 21:07:40 +0000

    added a new gift onto my new branch

--- hello.rb
+++ hello.rb
@@ -6,6 +6,7 @@ gift = [
           "a slice of stale wedding cake",
           "a medium-sized island off the coast of Sumatra",
           "a visual prosthesis",
+          "a brand new iPad"
          ]
          
 
